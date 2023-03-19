70b6b1a227fcc408dea5690aee75b3cbe62144ec
Tang4748 <109551346+Tang4748@users.noreply.github.com>
2023-03-18 18:13:40 -0700

    Added mongoose and specified model in models.js
    connected models to index.js
    added mongoose to packages
    dictated how models will be added to moviesDB

---
+++ models.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+let movieSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true}, 
+    genre: {
+        name: String,
+        description: String
+    },
+    director: {
+        name: String
+    },
+    actors: [String],
+    image_path: String,
+    featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: false},
+    Birthday: Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
